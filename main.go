5937a19ab701beb4ad4440ad9ec79b95a955da35
田家杰 <tjj2937693919@outlook.com>
2024-08-03 19:59:27 +0800

    logrus改为zap

--- main.go
+++ main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	_ "forum/inits" // 空导入，初始化
-	"forum/internal/server"
-	"forum/pkg/globals"
 )
 
-func main() {
-	globals.Log.Info("info ")
-	globals.Log.Error("error")
-
-	//启动路由
-	server.Run()
-
-}
+//func main() {
+//	globals.Log.Info("info ")
+//	globals.Log.Error("error")
+//
+//	//启动路由
+//	server.Run()
+//
+//}
