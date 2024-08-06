c5c6e43f600cf2ec3de387aaf62c8e32c7046e0d
lizhuang <3174285493@qq.com>
2024-08-06 22:00:50 +0800

    8月6日22:00

--- main.go
+++ main.go
@@ -3,14 +3,10 @@ package main
 import (
 	_ "forum/inits" // 空导入，初始化
 	"forum/internal/server"
-	"forum/pkg/globals"
 )
 
 func main() {
-	globals.Log.Info("info ")
-	globals.Log.Error("error")
-
-	//启动路由
+	// 启动路由
 	server.Run()
 
 }
