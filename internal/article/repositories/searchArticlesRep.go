e0ecc4356a52095614fad00440c24f8fb7e2b028
田家杰（tianjiajie） <tjj2937693919@outlook.com>
2024-09-12 21:02:03 +0800

    temp

--- internal/article/repositories/searchArticlesRep.go
+++ internal/article/repositories/searchArticlesRep.go
@@ -5,6 +5,7 @@ import (
 	"forum/internal/article/requests"
 	"forum/internal/models"
 	"forum/pkg/globals"
+	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
@@ -67,92 +68,109 @@ func SearchArticlesRep(db *gorm.DB, req *requests.ReqSearch) (articles []models.
 // @return       data
 // @return       err
 func SearchArticlesListRep(db *gorm.DB, req *requests.ArticleListReq) (data interface{}, err error) {
-	var res []requests.SearchArticleListRes
+	var articleList []requests.ArcList
 
-	query := db.Model(&models.Article{})
-	//.Preload("User")
-	query.Select(
-		"sw_articles.id, " +
-			"title, " +
-			"article_condition, " +
-			"views_count, " +
-			"likes_count, " +
-			"collections_count, " +
-			"comments_count, " +
-			"sw_articles.heat, " +
-			"sw_users.nickname, " +
-			"sw_articles.published_at, " +
-			"sw_articles.updated_at, " +
-			"sw_tags.id AS tag_id, " +
-			"sw_tags.name AS tag_name").
-		Joins("LEFT JOIN sw_users ON sw_users.id = sw_articles.user_id").
-		Joins("LEFT JOIN sw_article_tags ON sw_article_tags.article_id = sw_articles.id").
-		Joins("LEFT JOIN sw_tags ON sw_tags.id = sw_article_tags.tag_id")
+	query := db.Model(&models.Article{}).Preload("Tags")
+	//query.Select(
+	//	"sw_articles.id, " +
+	//		"title, " +
+	//		"article_condition, " +
+	//		"views_count, " +
+	//		"likes_count, " +
+	//		"collections_count, " +
+	//		"comments_count, " +
+	//		"sw_articles.heat, " +
+	//		"sw_users.nickname, " +
+	//		//"sw_tags.id AS tag_id, " +
+	//		//"sw_tags.name AS tag_name, "
+	//		"sw_articles.published_at, " +
+	//		"sw_articles.updated_at")
+	//Joins("LEFT JOIN sw_users ON sw_users.id = sw_articles.user_id")
+	//Joins("LEFT JOIN sw_article_tags ON sw_article_tags.article_id = sw_articles.id").
+	//Joins("LEFT JOIN sw_tags ON sw_tags.id = sw_article_tags.tag_id")
 
 	// 状态
-	query = query.Where("article_condition = ?", req.ArticleCondition)
+	//query = query.Where("article_condition = ?", req.ArticleCondition)
 
 	// 时间
-	if !req.StartTime.IsZero() && !req.EndTime.IsZero() {
-		query = query.Where("published_at BETWEEN ? AND ?", req.StartTime, req.EndTime)
-	} else {
-		if !req.StartTime.IsZero() {
-			query = query.Where("published_at >= ?", req.StartTime)
-		}
-		if !req.EndTime.IsZero() {
-			query = query.Where("published_at <= ?", req.EndTime)
-		}
-	}
-
-	// 公开文章
-	query = query.Where("status = ?", "public")
-
-	// 关键字
-	if req.Title != "" {
-		query = query.Where("title LIKE ?", "%"+req.Title+"%")
-	}
-
-	// 标签id
-	if len(req.ArticleTags) > 0 {
-		query = query.Where("sw_article_tags.tag_id IN (?)", req.ArticleTags)
-	}
-
-	// 发布人用户名
-	if req.Username != "" {
-		query = query.Where("sw_users.nickname = ?", req.Username)
-	}
-
-	// 浏览量 点赞量 收藏量 评论数量 热度
-	if req.ViewsCount != 0 {
-		query = query.Where("views_count >= ?", req.ViewsCount)
-	}
-	if req.LikesCount != 0 {
-		query = query.Where("likes_count >= ?", req.LikesCount)
-	}
-	if req.CollectionsCount != 0 {
-		query = query.Where("collections_count >= ?", req.CollectionsCount)
-	}
-	if req.CommentsCount != 0 {
-		query = query.Where("comments_count >= ?", req.CommentsCount)
-	}
-	if req.Heat != 0 {
-		query = query.Where("sw_articles.heat >= ?", req.Heat)
-	}
-
-	// 分页
-	if req.Limit != 0 {
-		offset := (req.Page - 1) * req.Limit
-		query = query.Limit(req.Limit).Offset(offset)
-	}
+	//if !req.StartTime.IsZero() && !req.EndTime.IsZero() {
+	//	query = query.Where("published_at BETWEEN ? AND ?", req.StartTime, req.EndTime)
+	//} else {
+	//	if !req.StartTime.IsZero() {
+	//		query = query.Where("published_at >= ?", req.StartTime)
+	//	}
+	//	if !req.EndTime.IsZero() {
+	//		query = query.Where("published_at <= ?", req.EndTime)
+	//	}
+	//}
+	//
+	//// 公开文章
+	//query = query.Where("status = ?", "public")
+	//
+	//// 关键字
+	//if req.Title != "" {
+	//	query = query.Where("title LIKE ?", "%"+req.Title+"%")
+	//}
+	//
+	//// 标签id
+	//if len(req.ArticleTags) > 0 {
+	//	query = query.Where("sw_article_tags.tag_id IN (?)", req.ArticleTags)
+	//}
+	//
+	//// 发布人用户名
+	//if req.Username != "" {
+	//	query = query.Where("sw_users.nickname = ?", req.Username)
+	//}
+	//
+	//// 浏览量 点赞量 收藏量 评论数量 热度
+	//if req.ViewsCount != 0 {
+	//	query = query.Where("views_count >= ?", req.ViewsCount)
+	//}
+	//if req.LikesCount != 0 {
+	//	query = query.Where("likes_count >= ?", req.LikesCount)
+	//}
+	//if req.CollectionsCount != 0 {
+	//	query = query.Where("collections_count >= ?", req.CollectionsCount)
+	//}
+	//if req.CommentsCount != 0 {
+	//	query = query.Where("comments_count >= ?", req.CommentsCount)
+	//}
+	//if req.Heat != 0 {
+	//	query = query.Where("sw_articles.heat >= ?", req.Heat)
+	//}
+	//
+	//// 分页
+	//if req.Limit != 0 {
+	//	offset := (req.Page - 1) * req.Limit
+	//	query = query.Limit(req.Limit).Offset(offset)
+	//}
 
 	// 执行查询
-	if err = query.Find(&res).Error; err != nil {
+	if err = query.Find(&articleList).Error; err != nil {
 		globals.Log.Errorf("err = %s", err)
 		return // 结束函数执行
 	}
 
-	fmt.Println(res)
-	data = res
+	//articleMap := make(map[uint]requests.SearchArticleListRes)
+	//for _, article := range articleList {
+	//	_, exists := articleMap[article.ID]
+	//	if !exists {
+	//		var tags []requests.TagsRes
+	//
+	//		db.Model(&models.ArticleTag{}).
+	//			Select("sw_tags.id AS tag_id, "+
+	//				"sw_tags.name AS tag_name").
+	//			Joins("LEFT JOIN sw_tags ON sw_tags.id = sw_article_tags.tag_id").
+	//			Where("article_id = ?", article.ID).Find(&tags)
+	//
+	//		article.Tags = append(article.Tags, tags...)
+	//
+	//		articleMap[article.ID] = article
+	//	}
+	//}
+
+	//fmt.Println(articleList)
+	data = gin.H{"article_list": articleList}
 
 	return data, err
 }
--- internal/article/requests/articleManagementReq.go
+++ internal/article/requests/articleManagementReq.go
@@ -1,6 +1,10 @@
 package requests
 
-import "time"
+import (
+	"forum/internal/models"
+	"gorm.io/gorm"
+	"time"
+)
 
 // ArticleListReq
 // @Description: 获取文章列表
@@ -21,22 +25,38 @@ type ArticleListReq struct {
 }
 
 type SearchArticleListRes struct {
-	ID               uint     `json:"id"`                // 文章ID
-	Title            string   `json:"title"`             // 文章标题
-	ArticleCondition int      `json:"article_condition"` // 文章状态
-	ViewsCount       int      `json:"views_count"`       // 浏览数
-	LikesCount       int      `json:"likes_count"`       // 点赞数
-	CollectionsCount int      `json:"collections_count"` // 收藏数
-	CommentsCount    string   `json:"comments_count"`    // 评论数
-	Heat             int      `json:"heat"`              // 热度
-	Username         string   `json:"username"`          // 发布人用户名
-	PublishedAt      string   `json:"published_at"`      // 发布时间
-	UpdatedAt        string   `json:"updated_at"`        // 更新时间
-	TagID            []int    `json:"tag_id"`            // 标签ID
-	TagName          []string `json:"tag_name"`          // 标签名称
+	ID               uint         `json:"id"`                                 // 文章ID
+	Title            string       `json:"title"`                              // 文章标题
+	ArticleCondition int          `json:"article_condition"`                  // 文章状态
+	ViewsCount       int          `json:"views_count"`                        // 浏览数
+	LikesCount       int          `json:"likes_count"`                        // 点赞数
+	CollectionsCount int          `json:"collections_count"`                  // 收藏数
+	CommentsCount    int          `json:"comments_count"`                     // 评论数
+	Heat             int          `json:"heat"`                               // 热度
+	Username         string       `json:"username"`                           // 发布人用户名
+	PublishedAt      string       `json:"published_at"`                       // 发布时间
+	UpdatedAt        string       `json:"updated_at"`                         // 更新时间
+	Tags             []models.Tag `gorm:"many2many:article_tags" json:"tags"` // 标签
 }
 
 type TagsRes struct {
 	TagID   int    `json:"tag_id"`   // 标签ID
 	TagName string `json:"tag_name"` // 标签名称
+}
+
+type ArcList struct {
+	gorm.Model                    //ID CreatedAt UpdatedAt DeletedAt
+	UserID           uint         `json:"user_id" gorm:"index"`                                                // 作者ID，外键
+	Title            string       `json:"title" gorm:"not null"`                                               // 文章标题
+	CollectionsCount int          `json:"collections_count" gorm:"default:0"`                                  // 收藏数量
+	CommentsCount    int          `json:"comments_count" gorm:"default:0"`                                     // 评论数量
+	ViewsCount       int          `json:"views_count" gorm:"default:0"`                                        // 浏览量
+	Heat             int          `json:"heat" gorm:"default:0"`                                               // 文章热度
+	Status           string       `json:"status" gorm:"type:enum('draft','private','public');default:'draft'"` // 文章属性（草稿，私有，公开）
+	CategoryID       uint         `json:"category_id" gorm:"default:0;index"`                                  // 所属类目ID，外键
+	Summary          string       `json:"summary" gorm:"type:text"`                                            // 文章摘要
+	PublishedAt      *time.Time   `json:"published_at"`                                                        // 发布时间 可以为空(如草稿)
+	Content          string       `json:"content" gorm:"type:text;not null"`                                   // 文章内容
+	ArticleCondition int          `json:"article_condition"`                                                   // 是否封禁 1：正常 2：封禁
+	Tags             []models.Tag `gorm:"many2many:article_tags;joinForeignKey:article_id"`
 }
