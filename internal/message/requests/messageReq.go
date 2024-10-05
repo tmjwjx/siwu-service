518dcaffc46f786625369781af4615c0d0fb14dd
田家杰（tianjiajie） <tjj2937693919@outlook.com>
2024-10-05 20:13:29 +0800

    点赞消息

--- internal/message/requests/messageReq.go
+++ internal/message/requests/messageReq.go
@@ -8,10 +8,10 @@ type MessageReq struct {
 	Limit int `json:"limit" form:"limit"`
 }
 
-// LikeMessageRes
+// LikeAndCollectionMessageRes
 // @Description: 点赞消息响应
 // @Author tianjiajie 2024-10-05 17:32:06
-type LikeMessageRes struct {
+type LikeAndCollectionMessageRes struct {
 	UserId    uint   `json:"user_id"`
 	Nickname  string `json:"nickname"`
 	Path      string `json:"path"`
