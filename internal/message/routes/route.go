a2eba82516d2d9220e42cbbad49042926fe2173a
田家杰（tianjiajie） <tjj2937693919@outlook.com>
2024-10-11 09:36:58 +0800

    redis和server配置调整

--- internal/message/routes/route.go
+++ internal/message/routes/route.go
@@ -12,7 +12,7 @@ func Message(e *gin.Engine) {
 	{
 		messageGroup.Use(token.AuthMiddleware())
 		// 推送消息
-		messageGroup.GET("/push", controllers.MessagePushCtrl)
+		messageGroup.GET("/sse", controllers.MessagePushCtrl)
 		// 点赞消息
 		messageGroup.GET("/like", controllers.LikeMessageCtrl)
 		// 收藏消息
