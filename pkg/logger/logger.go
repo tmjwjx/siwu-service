e7ffc9551d74e42ab3ac538a5f36f2cd0243983b
田家杰（tianjiajie） <tjj2937693919@outlook.com>
2025-01-16 11:26:17 +0800

    自动创建日志文件夹

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -26,6 +26,12 @@ func GetEncoder() zapcore.Encoder {
 // @return       zapcore.WriteSyncer
 // @Author tianjiajie 2024-10-05 16:03:01
 func GetLogWriter(logPath, appName string) zapcore.WriteSyncer {
+	// 确保日志目录存在
+	if err := os.MkdirAll(logPath, os.ModePerm); err != nil {
+		fmt.Printf("failed to create log directory: %v\n", err)
+		return nil
+	}
+
 	currentDate := time.Now().Format("2006-01-02")
 	fileName := fmt.Sprintf("./%s/%s-%s.log", logPath, appName, currentDate)
 	file, _ := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
