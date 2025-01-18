b42ef819aacb7502958861c5d5123ee025431fd2
lizhuang <3174285493@qq.com>
2025-01-18 15:18:30 +0800

    1.18 15:18

--- internal/user/controllers/userManageCtrl.go
+++ internal/user/controllers/userManageCtrl.go
@@ -158,7 +158,8 @@ func Export(c *gin.Context) {
 	// 设置响应头，返回 Excel 文件
 	fileName := "users.xls"
 	c.Header("Content-Disposition", "attachment; filename="+fileName)
-	c.Header("Content-Type", "application/vnd.ms-excel")
+	// c.Header("Content-Type", "application/vnd.ms-excel")
+	c.Header("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
 	c.Header("Content-Transfer-Encoding", "binary")
 
 	// 业务逻辑
