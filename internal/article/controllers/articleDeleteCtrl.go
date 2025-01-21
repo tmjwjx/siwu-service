66ec6f951c46014ecb4fc24fadb7c7d5bcf79da1
田家杰（tianjiajie） <tjj2937693919@outlook.com>
2025-01-21 09:23:15 +0800

    用户删除自己的文章

--- internal/article/controllers/articleDeleteCtrl.go
+++ internal/article/controllers/articleDeleteCtrl.go
@@ -53,3 +53,33 @@ func DeleteArticlesCtrl(c *gin.Context) {
 	data := response.NewAppData(globals.StatusOK, "成功", nil)
 	response.Success(c, http.StatusOK, data)
 }
+
+// UserDeleteArticlesCtrl
+// @Description: 用户 删除文章
+// @param        c *gin.Context
+// @Author tianjiajie 2025-01-21 09:04:12
+func UserDeleteArticlesCtrl(c *gin.Context) {
+	// 初始化需要的变量
+	db := globals.DB
+
+	// 绑定查询参数到变量
+	articleId := c.Query("id")
+	userId, ok := c.Get("id")
+	if !ok {
+		data := response.NewAppErr(globals.StatusInternalServerError, nil, nil)
+		response.Failed(c, http.StatusInternalServerError, data)
+		return
+	}
+
+	// 进入业务层
+	err := logics.UserArticleDeleteLocal(db, articleId, userId.(uint))
+	if err != nil {
+		data := response.NewAppErr(globals.StatusInternalServerError, err, nil)
+		response.Failed(c, http.StatusInternalServerError, data)
+		return
+	}
+
+	// 返回响应
+	data := response.NewAppData(globals.StatusOK, "成功", nil)
+	response.Success(c, http.StatusOK, data)
+}
