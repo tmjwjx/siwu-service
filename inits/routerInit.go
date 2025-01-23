469459e37bd23e3d343ac6ea256d8758d35f53e6
lizhuang <3174285493@qq.com>
2025-01-23 09:51:09 +0800

    1.23 9:51 自定义了：CustomRecovery中间件。在发生 panic 时，将错误打印到日志中，并返回 500。

--- inits/routerInit.go
+++ inits/routerInit.go
@@ -2,6 +2,7 @@ package inits
 
 import (
 	"forum/pkg/globals"
+	"forum/pkg/recovery"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +13,7 @@ func RouterInit() {
 	// 初始化路由 Router
 	globals.Router = gin.Default()
 
-	// 某一个控制器报错，不影响整体
-	globals.Router.Use(gin.Recovery())
+	// 某一个控制器报错，不影响整体（在发生 panic 时，将错误打印到日志中，并返回 500。）
+	globals.Router.Use(recovery.CustomRecovery())
+
 }
