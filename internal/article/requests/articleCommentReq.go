34379502fd5b710b5295667ded116e682d5483da
田家杰（tianjiajie） <tjj2937693919@outlook.com>
2025-01-23 10:33:57 +0800

    评论发布接口的userId没有传，改为获取token的id 返回评论中，查询点赞状态

--- internal/article/requests/articleCommentReq.go
+++ internal/article/requests/articleCommentReq.go
@@ -57,8 +57,9 @@ type FirstComment struct {
 	RepliesCount int64  `json:"replies_count"`  // 回复数量
 	Path         string `json:"path"`           // 用户头像
 	CommentPath  string `json:"comment_path"`   // 用户发的评论中的图片
-	Status       int    `json:"status"`         // 判断用户对评论的点赞情况
+	Status       bool   `json:"status"`         // 判断用户对评论的点赞情况
 	ParentUserID uint   `json:"parent_user_id"` // 上一条评论的发布用户ID
+	//LikeStatus   bool `json:"like_status"`    // 判断用户对评论的点赞情况
 }
 
 type RepliesReq2 struct {
@@ -97,7 +98,7 @@ type DelComment struct {
 }
 
 type PraiseCount struct {
-	ID     uint `json:"id"`      // 评论ID
-	Status uint `json:"status"`  // 用于判断是增加还是减少点赞数量，(1 : 代表增加， 2 : 代表减少)
-	UserID uint `json:"user_id"` // 点赞人ID
+	ID     uint `json:"id"`     // 评论ID
+	Status uint `json:"status"` // 用于判断是增加还是减少点赞数量，(1 : 代表增加， 2 : 代表减少)
+	//UserID uint `json:"user_id"` // 点赞人ID
 }
