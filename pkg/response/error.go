5fe2288c4019b0ddb3f068f958ace3c13ba1aced
lizhuang <3174285493@qq.com>
2025-02-06 15:57:05 +0800

    2.6 15:56 登陆注册验证码模块：添加了中间件来验证数据

--- pkg/response/error.go
+++ pkg/response/error.go
@@ -1,15 +1,12 @@
 package response
 
 const (
-	// 2000
 	DataSuccess = "成功"
 
-	// 4000
 	ErrPasswordIsInvalid    = "密码不合法"
 	ErrPasswordIsWrong      = "密码错误"
 	ErrPasswordTwiceIsWrong = "两次输入的密码不一致"
 	ErrEmailIsInvalid       = "邮箱不合法"
-	ErrEmailIsUse           = "邮箱已被使用"
 	// ErrEmailIsNotExist         = "不存在该邮箱"
 	ErrVerifyCodeIsWrong       = "验证码错误"
 	ErrVerifyCodeIsExpired     = "验证码已失效"
@@ -18,13 +15,14 @@ const (
 	ErrPageRangeIsWrong        = "page 范围错误"
 	ErrLimitRangeIsWrong       = "limit 范围错误"
 
-	// 5000
-	ErrUserIdNotExist       = "用户id不存在"
-	ErrEmailNotExist        = "用户邮箱不存在"
-	ErrBindDataIsWrong      = "绑定数据错误"
-	ErrTypeAssertionFail    = "类型断言失败"
-	ErrGetReqIsWrong        = "get req 错误"
-	ErrUnableFindUserAvatar = "无法找到用户头像"
-	ErrModelNotPointer      = "数据模型不是指针类型"
-	ErrInsertDataFail       = "插入数据失败"
+	ErrEmailIsUse              = "邮箱已被使用"
+	ErrUserIdNotExist          = "用户id不存在"
+	ErrEmailNotExist           = "用户邮箱不存在"
+	ErrEmailAnPasswordNotMatch = "邮箱和密码不符合"
+	ErrBindDataIsWrong         = "绑定数据错误"
+	ErrTypeAssertionFail       = "类型断言失败"
+	ErrGetReqIsWrong           = "get req 错误"
+	ErrUnableFindUserAvatar    = "无法找到用户头像"
+	ErrModelNotPointer         = "数据模型不是指针类型"
+	ErrInsertDataFail          = "插入数据失败"
 )
