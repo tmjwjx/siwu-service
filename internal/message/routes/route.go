7df08a0c913ad7712ba142bdf26cc95189fad3cc
田家杰（tianjiajie） <tjj2937693919@outlook.com>
2025-02-12 22:00:05 +0800

    获取未读点赞消息数量

--- internal/message/routes/route.go
+++ internal/message/routes/route.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Message(e *gin.Engine) {
-
+	
 	messageGroup := e.Group("/message")
 	{
 		messageGroup.Use(token.AuthMiddleware())
@@ -17,15 +17,23 @@ func Message(e *gin.Engine) {
 		messageGroup.GET("/like", controllers.LikeMessageCtrl)
 		// 收藏消息
 		messageGroup.GET("/collection", controllers.CollectionMessageCtrl)
-
 		// 关注消息
 		messageGroup.GET("/follow", controllers.FollowMessageCtrl)
-
 		// 评论消息
 		messageGroup.GET("/comment", controllers.CommentMesCtrl)
-
 		// 评论点赞消息
 		messageGroup.GET("/comment_like", controllers.CommentLikeMesCtrl)
+		
+		// 未读点赞消息数量
+		messageGroup.GET("/like_unread", controllers.LikeUnreadCtrl)
+		// 未读收藏消息数量
+		messageGroup.GET("/like_unread", controllers.LikeUnreadCtrl)
+		// 未读评论消息数量
+		messageGroup.GET("/like_unread", controllers.LikeUnreadCtrl)
+		// 未读关注消息数量
+		messageGroup.GET("/like_unread", controllers.LikeUnreadCtrl)
+		// 未读评论点赞消息数量
+		messageGroup.GET("/like_unread", controllers.LikeUnreadCtrl)
 	}
-
+	
 }
