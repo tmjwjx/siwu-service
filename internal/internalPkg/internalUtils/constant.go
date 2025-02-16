fb992ea7a26058c683c3c3d586937262d97cb62f
lizhuang <3174285493@qq.com>
2025-02-16 17:36:06 +0800

    修改了用户的随机名字

--- internal/internalPkg/internalUtils/constant.go
+++ internal/internalPkg/internalUtils/constant.go
@@ -6,19 +6,21 @@ import (
 
 // 验证码信息常量
 const (
-	VerifyCodeSubject           = "验证码"            // 验证码主题
-	VerifyCodeLen               = 6                // 验证码长度
-	VerifyCodeEffectiveDuration = 10 * time.Minute // 验证码有效时长
-	VerifyCodeCoolTime          = 1 * time.Minute  // 发送验证码的冷却时间
+	VerifyCodeSubject           = "验证码"                                                            // 验证码主题
+	VerifyCodeLen               = 6                                                                // 验证码长度
+	CustomAlphabetVerifyCode    = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" // 自定义字母表
+	VerifyCodeEffectiveDuration = 10 * time.Minute                                                 // 验证码有效时长
+	VerifyCodeCoolTime          = 1 * time.Minute                                                  // 发送验证码的冷却时间
 )
 
 // 用户默认常量
 const (
-	UserNameLen int = 8 // 用户默认姓名长度（随机姓名）
+	UserNameLen            int = 8            // 用户默认姓名长度（随机姓名）
+	CustomAlphabetNickname     = "1234567890" // 自定义字母表
 )
 
 // 默认图片路径
-//const (
+// const (
 //	// UserDefaultImage 默认用户头像路径
 //	UserDefaultImage = "user_default_head_image.png"
 //	// ArticleDefaultImage 默认文章图片路径
@@ -29,7 +31,7 @@ const (
 //	AdvertisementDefaultImage = "/images/advertisement_default_image.png"
 //	CommentDefaultImage       = ""
 //	CategoryDefaultImage      = ""
-//)
+// )
 
 // 用于判断广告是否被使用
 const (
