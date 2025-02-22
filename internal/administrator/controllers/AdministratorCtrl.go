e6cc3bf5b5eedf689d1643ae2b13ef9c95a9bf56
田家杰（tianjiajie） <tjj2937693919@outlook.com>
2025-02-22 08:33:11 +0800

    修改管理员信息、获取管理员列表、获取管理员详细信息、获取图片逻辑修改、重置密码

--- internal/administrator/controllers/AdministratorCtrl.go
+++ internal/administrator/controllers/AdministratorCtrl.go
@@ -40,14 +40,148 @@ import (
 	response.Success(c, http.StatusOK, data)
 */
 
-// DeleteAdministrator
+// UpdateAdministratorCtrl
+// @Description: 编辑管理员信息
+// @param        c *gin.Context
+// @Author tianjiajie 2025-02-21 17:42:20
+func UpdateAdministratorCtrl(c *gin.Context) {
+	// 初始化需要的变量
+	db := globals.DB
+	req := requests.UpdateAdministratorReq{}
+	id, ok := c.Get("id")
+	globals.Log.Info(id)
+	if !ok {
+		globals.Log.Errorf("身份验证失败")
+		data := response.NewAppErr(globals.StatusBadRequest, errors.New("获取id失败"), nil)
+		response.Failed(c, http.StatusBadRequest, data)
+		return
+	}
+
+	// 绑定查询参数到 req 变量
+	if err := c.ShouldBind(&req); err != nil {
+		// 日志记录错误信息
+		globals.Log.Errorf("绑定req失败 err = %s", err)
+		// 返回错误响应
+		data := response.NewAppErr(globals.StatusBadRequest, err, nil)
+		response.Failed(c, http.StatusBadRequest, data)
+		return // 结束函数执行
+	}
+
+	// 进入业务层
+	err := logics.UpdateAdministratorLogic(db, req, id)
+	if err != nil {
+		globals.Log.Errorf("获取管理员信息失败 err = %s", err)
+		data := response.NewAppErr(globals.StatusInternalServerError, err, nil)
+		response.Failed(c, http.StatusInternalServerError, data)
+		return
+	}
+
+	// 返回响应
+	data := response.NewAppData(globals.StatusOK, "成功", nil)
+	response.Success(c, http.StatusOK, data)
+
+}
+
+// ResetAdministratorPasswordCtrl
+// @Description: 重置管理员密码
+// @param        c *gin.Context
+// @Author tianjiajie 2025-02-21 21:27:59
+func ResetAdministratorPasswordCtrl(c *gin.Context) {
+	// 初始化需要的变量
+	db := globals.DB
+	req := requests.AdministratorReq{}
+
+	// 绑定查询参数到 req 变量
+	if err := c.ShouldBind(&req); err != nil {
+		// 日志记录错误信息
+		globals.Log.Errorf("绑定req失败 err = %s", err)
+		// 返回错误响应
+		data := response.NewAppErr(globals.StatusBadRequest, err, nil)
+		response.Failed(c, http.StatusBadRequest, data)
+		return // 结束函数执行
+	}
+
+	// 进入业务层
+	err := logics.ResetAdministratorPasswordLogic(db, req)
+	if err != nil {
+		globals.Log.Errorf("获取管理员信息失败 err = %s", err)
+		data := response.NewAppErr(globals.StatusInternalServerError, err, nil)
+		response.Failed(c, http.StatusInternalServerError, data)
+		return
+	}
+
+	// 返回响应
+	data := response.NewAppData(globals.StatusOK, "成功", nil)
+	response.Success(c, http.StatusOK, data)
+
+}
+
+// GetAdministratorInfoCtrl
+// @Description: 查询管理员详情
+// @param        c *gin.Context
+// @Author tianjiajie 2025-02-21 21:19:57
+func GetAdministratorInfoCtrl(c *gin.Context) {
+	// 初始化需要的变量
+	db := globals.DB
+
+	// 绑定查询参数到 req 变量
+	id := c.Query("id")
+
+	// 进入业务层
+	administratorInfo, err := logics.GetAdministratorInfoLogic(db, id)
+	if err != nil {
+		globals.Log.Errorf("获取管理员信息失败 err = %s", err)
+		data := response.NewAppErr(globals.StatusInternalServerError, err, nil)
+		response.Failed(c, http.StatusInternalServerError, data)
+		return
+	}
+
+	// 返回响应
+	data := response.NewAppData(globals.StatusOK, "成功", administratorInfo)
+	response.Success(c, http.StatusOK, data)
+}
+
+// GetAdministratorListCtrl
+// @Description: 查询管理员列表
+// @param        c *gin.Context
+// @Author tianjiajie 2025-02-21 20:30:46
+func GetAdministratorListCtrl(c *gin.Context) {
+	// 初始化需要的变量
+	db := globals.DB
+	req := requests.GetAdministratorListReq{}
+
+	// 绑定查询参数到 req 变量
+	if err := c.ShouldBind(&req); err != nil {
+		// 日志记录错误信息
+		globals.Log.Errorf("绑定req失败 err = %s", err)
+		// 返回错误响应
+		data := response.NewAppErr(globals.StatusBadRequest, err, nil)
+		response.Failed(c, http.StatusBadRequest, data)
+		return // 结束函数执行
+	}
+
+	// 进入业务层
+	administratorList, err := logics.GetAdministratorListLogic(db, req)
+	if err != nil {
+		globals.Log.Errorf("获取管理员列表失败 err = %s", err)
+		data := response.NewAppErr(globals.StatusInternalServerError, err, nil)
+		response.Failed(c, http.StatusInternalServerError, data)
+		return
+	}
+
+	// 返回响应
+	data := response.NewAppData(globals.StatusOK, "成功", administratorList)
+	response.Success(c, http.StatusOK, data)
+}
+
+// DeleteAdministratorCtrl
 // @Description: 删除管理员
 // @param        c *gin.Context
 // @Author tianjiajie 2025-02-21 14:59:46
-func DeleteAdministrator(c *gin.Context) {
+func DeleteAdministratorCtrl(c *gin.Context) {
 	// 初始化需要的变量
 	db := globals.DB
-	req := requests.DeleteAdministratorReq{}
+	req := requests.AdministratorReq{}
 	id, ok := c.Get("id")
 	globals.Log.Info(id)
 	if !ok {
--- internal/administrator/logics/AdministratorLogic.go
+++ internal/administrator/logics/AdministratorLogic.go
@@ -4,11 +4,104 @@ import (
 	"errors"
 	"forum/internal/administrator/repositories"
 	"forum/internal/administrator/requests"
+	"forum/internal/internalPkg/internalUtils"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
-func DeleteAdministratorLogic(db *gorm.DB, req requests.DeleteAdministratorReq) (err error) {
+// UpdateAdministratorLogic
+// @Description: 编辑管理员信息
+// @param        db *gorm.DB
+// @param        req requests.UpdateAdministratorReq
+// @return       data
+// @return       err
+// @Author tianjiajie 2025-02-21 21:56:20
+func UpdateAdministratorLogic(db *gorm.DB, req requests.UpdateAdministratorReq, id any) (err error) {
+	//// 身份验证
+	//if id != req.ID {
+	//	return errors.New("不是本人操作")
+	//}
+
+	// 编辑管理员信息
+	err = repositories.UpdateAdministratorRep(db, req)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// ResetAdministratorPasswordLogic
+// @Description: 重置管理员密码
+// @param        db *gorm.DB
+// @param        req requests.ResetAdministratorPasswordReq
+// @return       err
+// @Author tianjiajie 2025-02-21 21:31:18
+func ResetAdministratorPasswordLogic(db *gorm.DB, req requests.AdministratorReq) (err error) {
+	// 重置管理员密码
+	err = repositories.ResetAdministratorPasswordRep(db, req)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetAdministratorInfoLogic
+// @Description: 查询管理员详情
+// @param        db *gorm.DB
+// @param        id string
+// @return       data
+// @return       err
+// @Author tianjiajie 2025-02-21 21:21:48
+func GetAdministratorInfoLogic(db *gorm.DB, id string) (data interface{}, err error) {
+	// 查询管理员详情
+	administrator, err := repositories.GetAdministratorInfoRep(db, id)
+	if err != nil {
+		return nil, err
+	}
+
+	// 获取头像
+	str, _ := internalUtils.GetImages(db, "administrator", administrator.ID)
+	if len(*str) >= 1 {
+		administrator.Avatar = (*str)[0]
+	}
+
+	data = gin.H{"administrator": administrator}
+	return data, nil
+}
+
+// GetAdministratorListLogic
+// @Description: 查询管理员列表
+// @param        db *gorm.DB
+// @param        req requests.GetAdministratorListReq
+// @return       data
+// @return       err
+// @Author tianjiajie 2025-02-21 20:33:41
+func GetAdministratorListLogic(db *gorm.DB, req requests.GetAdministratorListReq) (data interface{}, err error) {
+	// 查询管理员列表
+	administratorList, err := repositories.GetAdministratorListRep(db, req)
+	if err != nil {
+		return nil, err
+	}
+
+	// 获取头像
+	for i := range administratorList {
+		str, _ := internalUtils.GetImages(db, "administrator", administratorList[i].ID)
+		if len(*str) >= 1 {
+			administratorList[i].Avatar = (*str)[0]
+		}
+	}
+
+	data = gin.H{"administrator_list": administratorList}
+	return data, nil
+}
+
+// DeleteAdministratorLogic
+// @Description: 删除管理员
+// @param        db *gorm.DB
+// @param        req requests.AdministratorReq
+// @return       err
+// @Author tianjiajie 2025-02-21 20:33:31
+func DeleteAdministratorLogic(db *gorm.DB, req requests.AdministratorReq) (err error) {
 	// 删除管理员
 	err = repositories.DeleteAdministratorRep(db, req)
 	if err != nil {
