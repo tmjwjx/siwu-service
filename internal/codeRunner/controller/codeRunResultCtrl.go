85ccb966a52973357c609d440cef4a2b8153fa82
ningzhaoxing <144292794+ningzhaoxing@users.noreply.github.com>
2025-03-13 20:36:54 +0800

    add CodeRunnerReq.go

---
+++ internal/codeRunner/controller/codeRunResultCtrl.go
@@ -0,0 +1,8 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+// GetCodeRunResultCtrl 从codeRunner服务中获取代码运行结果
+func GetCodeRunResultCtrl(c *gin.Context) {
+
+}
--- internal/codeRunner/requests/CodeRunnerReq.go
+++ internal/codeRunner/requests/CodeRunnerReq.go
@@ -1,1 +1,7 @@
 package requests
+
+type CodeRunnerReq struct {
+	Id       string `json:"id"`       // 代码块id
+	Language string `json:"language"` // 代码语言
+	CodeArea string `json:"codeArea"` // 代码块
+}
