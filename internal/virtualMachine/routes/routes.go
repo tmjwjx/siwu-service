1e483526d69e8cd90311c0b03bf5e45a4c35f99f
田家杰（tianjiajie） <tjj2937693919@outlook.com>
2025-03-17 08:55:08 +0800

    vm

--- internal/virtualMachine/routes/routes.go
+++ internal/virtualMachine/routes/routes.go
@@ -11,16 +11,19 @@ import (
 // @param        e *gin.Engine
 // @Author tianjiajie 2025-03-15 16:13:45
 func VirtualMachine(e *gin.Engine) {
-	
+
 	vmGroup := e.Group("/vm")
 	// 不需要 token 校验
 	{
-		
+		// pve回调api
+
+		// 创建虚拟机回调
+		vmGroup.POST("/create_callback", controllers.CreateVMCallback)
 	}
 	// token 校验
 	vmGroup.Use(token.AuthMiddleware())
 	{
 		vmGroup.POST("/create", controllers.CreateVM)
 	}
-	
+
 }
