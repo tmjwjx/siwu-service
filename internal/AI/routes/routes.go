118e95c676ab6f9b868bd0628c78fba7a282f0cb
Wangyulong <w137546zxc@qq.com>
2025-03-20 09:10:12 +0800

    添加了部分注释

--- internal/AI/routes/routes.go
+++ internal/AI/routes/routes.go
@@ -8,9 +8,16 @@ import (
 func AIRouters(e *gin.Engine) {
 	articleRouter := e.Group("/AI/extract")
 	{
+		// 第一次获取文章的摘要、总结、标签
 		articleRouter.GET("/get_info_first", controllers.GetArticleInfoFirstCtrl)
+
+		// 将文章的ID保存到相应的记录中
 		articleRouter.POST("/save_article_id", controllers.SaveArticleIDCtrl)
+
+		// 非首次获取文章的摘要、总结、标签
 		articleRouter.GET("/get_info", controllers.GetArticleInfoCtrl)
+
+		// 删除文章相关信息
 		articleRouter.DELETE("/del_info", controllers.DelArticleInfoCtrl)
 	}
 }
