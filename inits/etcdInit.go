68e042fcbe51c0df4b323f24354d3e2c6f055fcc
Wangyulong <w137546zxc@qq.com>
2025-04-05 08:05:39 +0800

    修改配置文件，修改第一次获取文章信息接口的请求方式。

--- inits/etcdInit.go
+++ inits/etcdInit.go
@@ -3,11 +3,17 @@ package inits
 import (
 	"forum/pkg/etcd"
 	"forum/pkg/globals"
+	"github.com/spf13/viper"
 	"log"
 )
 
-func InitEtcd(addr []string) {
-	etcd, err := etcd.NewEtcdClient(addr)
+func InitEtcd() {
+
+	if err := viper.UnmarshalKey("etcd", &globals.AppConfig.Etcd); err != nil {
+		globals.Log.Fatalf("Run -> 无法解码为结构: %s", err)
+	}
+
+	etcd, err := etcd.NewEtcdClient([]string{globals.AppConfig.Etcd.Endpoints})
 	if err != nil {
 		log.Println("InitEtcd() -> 创建 etcdService 失败, err = ", err)
 	}
