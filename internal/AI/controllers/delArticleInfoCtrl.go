161e9f6393ac8a8527f087d811c4e28a21540054
Wangyulong <w137546zxc@qq.com>
2025-04-09 21:04:34 +0800

    修改了删除文章接口中返给前端的信息

--- internal/AI/controllers/delArticleInfoCtrl.go
+++ internal/AI/controllers/delArticleInfoCtrl.go
@@ -15,7 +15,7 @@ func DelArticleInfoCtrl(c *gin.Context) {
 	//接收前端的代码
 	var req requests.DelArticleInfoReq
 	if err := c.ShouldBindJSON(&req); err != nil {
-		response.Failed(c, http.StatusBadRequest, response.NewAppErr(globals.StatusBadRequest, fmt.Errorf("SaveArticleIDCtrl -> %v", err), nil))
+		response.Failed(c, http.StatusBadRequest, response.NewAppErr(globals.StatusBadRequest, fmt.Errorf("DelArticleInfoCtrl -> %v", err), nil))
 		return
 	}
 
@@ -25,7 +25,7 @@ func DelArticleInfoCtrl(c *gin.Context) {
 	}
 	token, err := etcdToken.GetToken()
 	if err != nil {
-		response.Failed(c, http.StatusInternalServerError, response.NewAppErr(globals.StatusInternalServerError, fmt.Errorf("SaveArticleIDCtrl -> %v", err), nil))
+		response.Failed(c, http.StatusInternalServerError, response.NewAppErr(globals.StatusInternalServerError, fmt.Errorf("DelArticleInfoCtrl -> %v", err), nil))
 		return
 	}
 
@@ -35,7 +35,7 @@ func DelArticleInfoCtrl(c *gin.Context) {
 
 	res, err := d.DelArticleInfoLogic(&req)
 	if err != nil {
-		response.Failed(c, http.StatusInternalServerError, response.NewAppErr(globals.StatusInternalServerError, fmt.Errorf("SaveArticleIDCtrl -> %v", err), nil))
+		response.Failed(c, http.StatusInternalServerError, response.NewAppErr(globals.StatusInternalServerError, fmt.Errorf("DelArticleInfoCtrl -> %v", err), nil))
 		return
 	}
 
