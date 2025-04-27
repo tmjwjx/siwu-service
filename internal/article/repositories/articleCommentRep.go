2662cef03b6c92116de7f925921adfa4276fda24
田家杰（tianjiajie） <tjj2937693919@outlook.com>
2025-04-27 15:24:41 +0800

    sse,消息推送，旧格式删除

--- internal/article/repositories/articleCommentRep.go
+++ internal/article/repositories/articleCommentRep.go
@@ -102,13 +102,13 @@ func InsertCommentRep(userId uint, articleCommentReq *requests.ArticleCommentReq
 		}
 
 		// 评论通知
-		internalUtils.MessagePush("comment", fmt.Sprintf("%v", aUserID))
+		//internalUtils.MessagePush("comment", fmt.Sprintf("%v", aUserID))
 		jsonData, _ := json.Marshal(gin.H{"content": "comment"})
 		internalUtils.MessagePush2(string(jsonData), fmt.Sprintf("%v", aUserID), globals.NoticeType, "")
 	} else {
 		// 如果不是顶级评论，返回回复的评论的发布者ID
 		// 评论通知
-		internalUtils.MessagePush("comment", fmt.Sprintf("%v", articleCommentReq.ParentUserID))
+		//internalUtils.MessagePush("comment", fmt.Sprintf("%v", articleCommentReq.ParentUserID))
 		jsonData, _ := json.Marshal(gin.H{"content": "comment"})
 		internalUtils.MessagePush2(string(jsonData), fmt.Sprintf("%v", articleCommentReq.ParentUserID), globals.NoticeType, "")
 	}
@@ -641,7 +641,7 @@ func UpdatePraiseCountRep(req *requests.PraiseCount, db *gorm.DB, userId uint) e
 					var comment models.ArticleComment
 					err = db.Model(&models.ArticleComment{}).Select("user_id").Where("id = ?", req.ID).First(&comment).Error
 					// 评论点赞通知
-					internalUtils.MessagePush("comment_like", strconv.Itoa(int(comment.UserID)))
+					//internalUtils.MessagePush("comment_like", strconv.Itoa(int(comment.UserID)))
 					jsonData, _ := json.Marshal(gin.H{"content": "comment_like"})
 					internalUtils.MessagePush2(string(jsonData), strconv.Itoa(int(comment.UserID)), globals.NoticeType, "")
 
